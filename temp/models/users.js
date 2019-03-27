5c452b05dba141e85c82eeaa1a6ff646207b0424
Prerana Singh <prernasingh726@gmail.com>
2019-03-28 00:01:09 +0530

    Created nav bar sample

--- temp/models/users.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const db = require('./connection')
-const api = require('./routes/api')
-const view = require('./routes/view')
-
-console.log(db,"db");
